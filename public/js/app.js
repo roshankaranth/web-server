2adefe277fbe0fabc84c60e984904e8d0a08f73d
Roshan Karanth <roshankaranth@gmail.com>
2023-06-24 10:05:14 +0530

    Created a search form

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,19 @@
 console.log("client side js file loaded")
 
 
+const weather_form = document.querySelector('form')
+const search = document.querySelector('input')
+weather_form.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data)
-        }
+    const location = search.value
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data)
+            }
+        })
     })
 })
