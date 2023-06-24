896caf2b47c08652d45cae3cd0c151c8ddd4fffb
Roshan Karanth <roshankaranth@gmail.com>
2023-06-24 10:28:49 +0530

    Wired up user interface

--- public/js/app.js
+++ public/js/app.js
@@ -3,17 +3,25 @@ console.log("client side js file loaded")
 
 const weather_form = document.querySelector('form')
 const search = document.querySelector('input')
+const msg1 = document.querySelector('#message-1')
+const msg2 = document.querySelector('#message-2')
+
 weather_form.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
+    msg1.textContent = 'loading..'
+    msg2.textContent = ''
     fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log(data.error)
+                msg1.textContent = data.error
             } else {
-                console.log(data)
+                msg1.textContent = `${data.location.name} ${data.location.region}, ${data.location.country}`
+                msg2.textContent = `Today's temperature is ${data.current.temp_c} and the humidity is ${data.current.humidity}.  `
+
             }
         })
     })
 })
+
